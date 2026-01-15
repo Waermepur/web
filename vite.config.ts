e077b30658f2adab93feddb34a07f7dd043b1f01
Waermepur <info@xn--wrmepur-5wa.com>
2026-01-15 16:33:51 +0100

    Change base path for Vite configuration
    Updated base path for deployment configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/web/', // 🔥 WICHTIG für GitHub Pages
+  base: '/web/', Neu: base: '/',
   plugins: [react()],
 })
